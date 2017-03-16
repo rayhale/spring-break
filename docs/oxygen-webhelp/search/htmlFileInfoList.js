a2d0f4bcb11c4403d700addf6c725da0a9c82c26
Rachel Hale <rayh17@vt.edu>
2017-03-16 10:18:19 -0400

    moved my files

---
+++ docs/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,6 @@
+fil = new Array();
+fil["0"]= "concepts/Spring Break.html@@@Spring Break: My first time in Key West@@@A group of friends and I are roadtripping down to Key west for Spring Break 2017!...";
+fil["1"]= "tasks/Driving.html@@@How we are getting to Key West@@@Because flights are so expensive during spring break season, we decided to make the long 20 hour drive down to Key West...";
+fil["2"]= "tasks/Room.html@@@Where we&apos;re staying in Key West@@@We are staying at a motel...";
+fil["3"]= "tasks/funding.html@@@How we&apos;re paying for Spring Break@@@We want to spend as little as possible for this trip because we are broke college students...";
+fil["4"]= "troubleshooting/troubleshooting.html@@@Troubleshooting@@@I dont know how to get down to Key West. If you don&apos;t have a way to get down to Key West, you can go online and buy a plane ticket. You don&apos;t have money for a flight. Save money by driving down to Key...";
---
+++ docs/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,8 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concepts/Spring Break.html";
+fl["1"]= "tasks/Driving.html";
+fl["2"]= "tasks/Room.html";
+fl["3"]= "tasks/funding.html";
+fl["4"]= "troubleshooting/troubleshooting.html";
+var doStem = false;searchLoaded = true;
